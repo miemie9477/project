35e158af7c8b1f99f567a2c81894409fd962a9d2
miemie9477 <jelena0625@gmail.com>
2024-05-02 23:10:37 +0800

    Add some revise for testing pwsh

--- src/component/pages/00OtherPage/RegisterPage.js
+++ src/component/pages/00OtherPage/RegisterPage.js
@@ -32,53 +32,59 @@ const RegisterForm = () =>{
         <div>
             <Container>
                 <Row>
-                <Col>
-                </Col>
+                    <Col>
+                    </Col>
 
-                <Col>
-                    <form name="" action="" method="POST">
-                    <div className="">
-                        <Form.Label htmlFor="inputName"><b>真實姓名</b></Form.Label>
-                        <Form.Control
-                            size={10}
-                            type="name"
-                            id="registerName"
-                        />
-                        
-                        <Form.Label htmlFor="registAccount"><b>帳號</b></Form.Label>
-                        <Form.Control
-                            size={10}
-                            type="account"
-                            id="registAccount"
-                        />
-                        <Form.Label htmlFor="registerPassword"><b>密碼</b></Form.Label>
-                        <Form.Control
-                            size={10}
-                            type="password"
-                            id="registerPassword"
-                        />
-                        <Form.Label htmlFor="registerPId"><b>身分證字號</b></Form.Label>
-                        <Form.Control
-                            size={10}
-                            type="text"
-                            id="registerPId"
-                        />
-                        <Form.Label htmlFor="registerEmail"><b>電子郵件</b></Form.Label>
-                        <Form.Control
-                            size={10}
-                            type="text"
-                            id="registerEmail"
-                        />
-                        <Form.Label htmlFor="registerAddress"><b>聯絡住址</b></Form.Label>
-                        <Form.Control
-                            size={10}
-                            type="text"
-                            id="registerAddress"
-                        />
-                    </div>
-                    </form>
-                </Col>
-            </Row>
+                    <Col>
+                        <form name="" action="" method="POST">
+                        <div className="">
+                            <Form.Label htmlFor="inputName"><b>真實姓名</b></Form.Label>
+                            <Form.Control
+                                size={10}
+                                type="name"
+                                id="registerName"
+                            />
+                            
+                            <Form.Label htmlFor="registAccount"><b>帳號</b></Form.Label>
+                            <Form.Control
+                                size={10}
+                                type="account"
+                                id="registAccount"
+                            />
+                            <Form.Label htmlFor="registerPassword"><b>密碼</b></Form.Label>
+                            <Form.Control
+                                size={10}
+                                type="password"
+                                id="registerPassword"
+                            />
+                            <Form.Label htmlFor="registerPId"><b>身分證字號</b></Form.Label>
+                            <Form.Control
+                                size={10}
+                                type="text"
+                                id="registerPId"
+                            />
+                            <Form.Label htmlFor="registerEmail"><b>電子郵件</b></Form.Label>
+                            <Form.Control
+                                size={10}
+                                type="text"
+                                id="registerEmail"
+                            />
+                            <Form.Label htmlFor="registerAddress"><b>聯絡住址</b></Form.Label>
+                            <Form.Control
+                                size={10}
+                                type="text"
+                                id="registerAddress"
+                            />
+                            <Form.Label htmlFor="registerPhone"><b>手機電話</b></Form.Label>
+                            <Form.Control
+                                size={10}
+                                type="text"
+                                id="registerPhone"
+                            />
+                        </div>
+                        </form>
+                    </Col>
+                </Row>
             </Container>
             <Button variant="danger">登入</Button>
         </div>
